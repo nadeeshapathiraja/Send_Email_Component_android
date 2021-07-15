bfd3c3eace61a381f9d2f428a98b8bbb50285c5b
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-07-15 23:02:02 +0530

    Add compile time errors

--- app/src/main/java/com/example/emailtestcomponent/MainActivity.java
+++ app/src/main/java/com/example/emailtestcomponent/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     //create variabels
     EditText email,message;
-    Button btn_send, btn_clear;
+    Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,42 +33,48 @@ public class MainActivity extends AppCompatActivity {
 
         email = findViewById(R.id.email);
         message = findViewById(R.id.message);
-
-        btn_send.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                final String userName = "pdncpathiraja@gmail.com";
-                final String password = "pdnc45325";
+            public void run() {
+                btn_send.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String userName = "pdncpathiraja@gmail.com";
+                        final String password = "pdnc45325";
 
-                String messageToSend = message.getText().toString();
+                        String messageToSend = message.getText().toString();
 
-                Properties prop = new Properties();
-                prop.put("mail.smtp.auth","true");
-                prop.put("mail.smtp.starttls.enable","true");
-                prop.put("mail.smtp.host","smtp.gmail.com");
-                prop.put("mail.smtp.port","587");
-                Session session = Session.getInstance(prop,
-                        new javax.mail.Authenticator() {
-                            @Override
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(userName, password);
-                            }
-                        });
-                try {
-                    Message message = new MimeMessage(session);
-                    message.setFrom( new InternetAddress(userName));
-                    message.setRecipients(Message.RecipientType.TO , InternetAddress.parse(email.getText().toString()));
-                    message.setSubject("Sending Email Without Open Gmail app");
-                    message.setText(messageToSend);
-                    Transport.send(message);
-                    Toast.makeText(getApplicationContext(), "Email Send Successfull", Toast.LENGTH_LONG).show();
-                }
-                catch (MessagingException e){
-                    throw new RuntimeException(e);
-                }
-                
+                        Properties prop = new Properties();
+                        prop.put("mail.smtp.auth","true");
+                        prop.put("mail.smtp.starttls.enable","true");
+                        prop.put("mail.smtp.host","smtp.gmail.com");
+                        prop.put("mail.smtp.port","587");
+                        Session session = Session.getInstance(prop,
+                                new javax.mail.Authenticator() {
+                                    @Override
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(userName, password);
+                                    }
+                                });
+                        try {
+                            Message message = new MimeMessage(session);
+                            message.setFrom( new InternetAddress(userName));
+                            message.setRecipients(Message.RecipientType.TO , InternetAddress.parse(email.getText().toString()));
+                            message.setSubject("Sending Email Without Open Gmail app");
+                            message.setText(messageToSend);
+                            Transport.send(message);
+                            Toast.makeText(getApplicationContext(), "Email Send Successfull", Toast.LENGTH_LONG).show();
+                        }
+                        catch (MessagingException e){
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                });
             }
         });
+
+
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
